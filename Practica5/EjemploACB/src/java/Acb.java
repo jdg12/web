40ab0b2e65c4994f1eb9bf920438ce3d665b8c4e
jesus dapena gomez <jesus dapena gomez>
2016-11-24 17:05:57 +0100

    Practica5

---
+++ Practica5/EjemploACB/src/java/Acb.java
@@ -0,0 +1,42 @@
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+import java.sql.*;
+
+public class Acb extends HttpServlet {
+
+    private ModeloDatos bd;
+
+    @Override
+    public void init(ServletConfig cfg) throws ServletException {
+        bd = new ModeloDatos();
+        bd.abrirConexion();
+    }
+
+    @Override
+    public void service(HttpServletRequest req,
+            HttpServletResponse res) throws ServletException, IOException {
+        HttpSession s = req.getSession(true);
+        String nombreP = (String) req.getParameter("txtNombre");
+        String nombre = (String) req.getParameter("R1");
+        if (nombre.equals("Otros")) {
+            nombre = (String) req.getParameter("txtOtros");
+        }
+        if (bd.existeJugador(nombre)) {
+            bd.actualizarJugador(nombre);
+        } else {
+            bd.insertarJugador(nombre);
+        }
+        s.setAttribute("nombreCliente", nombreP);
+// Llamada a la página jsp que nos da las gracias
+        res.sendRedirect(res.encodeRedirectURL("/ACB/TablaVotos.jsp"));
+    }
+
+    @Override
+    public void destroy() {
+        bd.cerrarConexion();
+        super.destroy();
+    }
+}
---
+++ Practica5/EjemploACB/src/java/ModeloDatos.java
@@ -0,0 +1,77 @@
+
+import java.util.*;
+import java.sql.*;
+
+public class ModeloDatos {
+
+    private Connection con;
+    private Statement set;
+    private ResultSet rs;
+
+    public void abrirConexion() {
+        String sURL = "jdbc:odbc:mvc";
+        try {
+//Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+//    con = DriverManager.getConnection(sURL,"","");
+            con = DriverManager.getConnection("jdbc:derby://localhost:1527/sample", "app", "app");
+            System.out.println("Se ha conectado");
+        } catch (Exception e) {
+            System.out.println("No se ha conectado");
+        }
+    }
+
+    public boolean existeJugador(String nombre) {
+        boolean existe = false;
+        String cad;
+        try {
+            set = con.createStatement();
+            rs = set.executeQuery("SELECT * FROM Jugadores");
+            while (rs.next()) {
+                cad = rs.getString("Nombre");
+                cad = cad.trim();
+                if (cad.compareTo(nombre.trim()) == 0) {
+                    existe = true;
+                }
+            }
+            rs.close();
+            set.close();
+        } catch (Exception e) {
+            System.out.println("No lee de la tabla");
+        }
+        return (existe);
+    }
+
+    public void actualizarJugador(String nombre) {
+        try {
+            set = con.createStatement();
+            set.executeUpdate(
+                    "UPDATE Jugadores SET votos=votos+1 WHERE nombre "
+                    + " LIKE '%" + nombre + "%'");
+            rs.close();
+            set.close();
+        } catch (Exception e) {
+            System.out.println("No modifica la tabla");
+        }
+    }
+
+    public void insertarJugador(String nombre) {
+        try {
+            set = con.createStatement();
+            set.executeUpdate("INSERT INTO Jugadores "
+                    + " (nombre,votos) VALUES ('" + nombre + "',1)");
+            rs.close();
+            set.close();
+        } catch (Exception e) {
+            System.out.println("No inserta en la tabla");
+        }
+    }
+
+    public void cerrarConexion() {
+        try {
+            con.close();
+        } catch (Exception e) {
+        }
+    }
+
+}
