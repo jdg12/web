a975d8b9f74186cc13e66adecacc36ac7a361445
jesus dapena gomez <jesus dapena gomez>
2016-11-24 17:05:28 +0100

    Ejemplo

---
+++ PracticaMVC-EjemploACB/src/java/Paquete/ACB.java
@@ -0,0 +1,37 @@
+package Paquete;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class ACB extends HttpServlet {
+
+    private ModeloDatos bd;
+
+    public void init(ServletConfig cfg) throws ServletException {
+        bd = new ModeloDatos();
+        bd.abrirConexion();
+    }
+
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        HttpSession s = req.getSession(true);
+        String nombreP = (String) req.getParameter("txtNombre");
+        String nombre = (String) req.getParameter("R1");
+        if (nombre.equals("Otros")) {
+            nombre = (String) req.getParameter("txtOtros");
+        }
+        if (bd.existeJugador(nombre)) {
+            bd.actualizarJugador(nombre);
+        } else {
+            bd.insertarJugador(nombre);
+        }
+        s.setAttribute("nombreCliente", nombreP);
+        // Llamada a la página jsp que nos da las gracias
+        res.sendRedirect(res.encodeRedirectURL("/PracticaMVC-EjemploACB/TablaVotos.jsp"));
+    }
+
+    public void destroy() {
+        bd.cerrarConexion();
+        super.destroy();
+    }
+}
