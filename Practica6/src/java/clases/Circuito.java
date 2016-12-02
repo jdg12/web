1830dba161ac35dbf7795ea42d86df63cd744970
jesus dapena gomez <jesus dapena gomez>
2016-12-02 15:14:19 +0100

    circuitojava

---
+++ Practica6/src/java/clases/Circuito.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author jesus
+ */
+public class Circuito {
+    private String nombre;
+    private String ciudad;
+    private String pais;
+    private int vueltas;
+    private double longitud;
+    private int curvas;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getVueltas() {
+        return vueltas;
+    }
+
+    public void setVueltas(int vueltas) {
+        this.vueltas = vueltas;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    public int getCurvas() {
+        return curvas;
+    }
+
+    public void setCurvas(int curvas) {
+        this.curvas = curvas;
+    }
+}
